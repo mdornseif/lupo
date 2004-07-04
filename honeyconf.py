6a534d258fe860cbe8064a0a417ac70cf4fa206d
md <md@2d70b15c-62de-0310-b45c-a2b26695ab02>
2004-07-04 20:22:22 +0000

    start of python rewrite
    git-svn-id: http://svn.23.nu/svn/repos/lupo/trunk@638 2d70b15c-62de-0310-b45c-a2b26695ab02

---
+++ honeyconf.py
@@ -0,0 +1,14 @@
+#!/usr/local/python
+
+
+# check if there is a new config | honeyconf | honeyd on the server
+# if yes:
+#  fetch it
+#  patch them
+#  restart honeyd if needed
+
+# checksum binaries
+
+# rotate logfiles
+
+# upload logfiles
